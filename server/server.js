60b1cde93ca0033b8cff6d957ae6770beb2db1a7
Mikael Kotkavuori <mikael.kotkavuori@gmail.com>
2019-03-11 13:37:52 +0200

    Trying fixes for socket io

--- server/server.js
+++ server/server.js
@@ -3,8 +3,7 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 
 const app = express();
-const http  = require('http').Server(app);
-const PORT = process.env.PORT || 8000;
+// const http  = require('http').Server(app);
 const routes = require('./routes/routes');
 const controller = require('./routes/controllers/restController');
 
@@ -16,12 +15,13 @@ const socketIO = require('socket.io');
 if(process.env.NODE_ENV === 'production'){
   //Static folder
   app.use(express.static(__dirname + '/public/'));
-
+  
   //Handle SPA
   app.get(/.*/, (req, res) => res.sendFile(__dirname + '/public/index.html'));
-
+  
 }
 
+const PORT = process.env.PORT || 8000;
 const server = express()
 .use(app)
 .listen(PORT, () => console.log(`Listening Socket on ${ PORT }`));
